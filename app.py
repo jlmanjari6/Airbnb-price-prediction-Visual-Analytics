e2185a018b0aacbeb004342268345ccafc90d247
Manjari <lk659879@dal.ca>
2019-11-30 13:53:16 -0400

    code formatting changes and comments added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import io
 import json
 
 import folium
+import matplotlib.pyplot as plt
 import pandas as pd
 import plotly
 import plotly.graph_objs as go
@@ -15,10 +16,10 @@ from sklearn import preprocessing
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from yellowbrick.regressor import ResidualsPlot
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
+# this is to find columns required for clustering
 df_entire = pd.read_csv('listings.csv')
 oneHotColumns = ['room_type']
 requiredColummns = oneHotColumns + ['name', 'latitude', 'longitude', 'price']
@@ -41,22 +42,31 @@ class PricePrediction:
 
 main_class_obj = MainClass()
 # **************************************************** Dashboard page **************************************************
+
+
+# route for home page
 @app.route('/', methods=['GET'])
 def render_home():
     neighbourhood_groups = []
     neighborhoods = []
-
+    # accessing cleaned csv file
     df = main_class_obj.cleaned_df
     lats2019 = df['latitude'].tolist()
     lons2019 = df['longitude'].tolist()
     locations = list(zip(lats2019, lons2019))
+
+    # generating folium map with fast marker clusters using latitudes and longitudes
     map1 = folium.Map(location=[55.585901, -105.750596], zoom_start=3.3)
     FastMarkerCluster(data=locations).add_to(map1)
+
+    # saving the html to render it on webpage
     map1.save(outfile="./static/dashboard-map5.html")
     return render_template('index.html', neighbourhood_groups=neighbourhood_groups, neighborhoods=neighborhoods)
 
-
 # ****************************************************** Trends page ***************************************************
+
+
+# route for trends page
 @app.route('/Trends', methods=['GET'])
 def render_trends():
     df = main_class_obj.cleaned_df
@@ -65,10 +75,13 @@ def render_trends():
     x = "bedrooms"
     y = "room_type"
     dfg = df[df.province == provinces[0]]
+
+    # generate plot for default options
     plot_generated = get_plot(df, dfg, x, y, provinces[0])
     return render_template('trends.html', provinces=provinces, plot=plot_generated)
 
 
+# this is to generate plot for selected user parameters
 def get_plot(df, dfg, x, y, selected_province):
     y_values = list(getattr(df, y).unique())
     x_values = list(getattr(df, x).unique())
@@ -91,6 +104,7 @@ def get_plot(df, dfg, x, y, selected_province):
     return graph_json
 
 
+# route to generate chart in runtime using the selected parameters
 @app.route('/GenerateChart', methods=['GET', 'POST'])
 def generate_plot():
     selected_province = request.form.get('provinceID')
@@ -99,14 +113,15 @@ def generate_plot():
     df = main_class_obj.cleaned_df
     provinces = df.province.unique()
     dfg = df[df.province == selected_province]
+
+    # generate plot
     plot_generated = get_plot(df, dfg, x, y, selected_province)
     return render_template('trends.html', plot=plot_generated, provinces=provinces, sel_x=x, sel_y=y ,
                            sel_province=selected_province)
 
-
 # ************************************************ Find Airbnbs page ***************************************************
 
-
+# to land on to Find airbnbs page with default options
 @app.route('/FindAirbnbs', methods=['GET'])
 def render_airbnbs():
     provinces = []
@@ -126,11 +141,13 @@ def render_airbnbs():
     # filtering room type
     r_df = n_df[(n_df.room_type == roomtypes[0])]
 
+    # generate folium map using default options
     response_map = generate_marker_latlong(r_df)
     return render_template('find-airbnbs.html', provinces=provinces, neighborhoods=neighborhoods, roomtypes = roomtypes
                            , response_map=response_map)
 
 
+# this is to generate folium map using list of airbnbs
 def generate_marker_latlong(r_df):
     latitudes_list = list(r_df.latitude)
     longitudes_list = list(r_df.longitude)
@@ -148,6 +165,7 @@ def generate_marker_latlong(r_df):
     return map2
 
 
+# this is to generate map using selected user options runtime
 @app.route('/GenerateMarkers', methods=['GET', 'POST'])
 def generate_markers():
     selected_province = request.form.get('provinceID')
@@ -165,6 +183,7 @@ def generate_markers():
                            sel_roomtype=selected_roomtype)
 
 
+# get filtered options based on selected user parameters
 def get_filtered_df():
     df = main_class_obj.cleaned_df
 
@@ -187,6 +206,7 @@ def get_filtered_df():
     return r_df, provinces, neighborhoods, roomtypes
 
 
+# handling Ajax request and response to take user selected province and provide corresponding neighbourhoods
 @app.route('/get_neighbourhood/<provinceID>')
 def get_neighbourhood(provinceID):
     df = main_class_obj.cleaned_df
@@ -195,6 +215,7 @@ def get_neighbourhood(provinceID):
     return jsonify(neighborhoods)
 
 
+# handling Ajax request and response to take user selected neighborhood and provide corresponding room types available
 @app.route('/get_roomtypes/<neighbourhoodID>')
 def get_roomtypes(neighbourhoodID):
     df = main_class_obj.cleaned_df
@@ -202,14 +223,17 @@ def get_roomtypes(neighbourhoodID):
     roomtypes = list(p_df.room_type.unique())
     return jsonify(roomtypes)
 
-
 # *********************************************** Predict price page ***************************************************
 
 
+# this is to fill dropdowns in predict price page using preprocessed df
 @app.route('/PricePrediction', methods=['GET'])
 def render_price_prediction():
+    # get pre processed df
     price_prediction_obj = PricePrediction()
     ndf = price_prediction_obj.preprocessed_df
+
+    # fill dropdowns
     provinces = ndf.province.unique()
     accommodates = sorted(ndf.accommodates.unique())
     bedrooms = sorted(ndf.bedrooms.unique())
@@ -218,6 +242,7 @@ def render_price_prediction():
                            minimum_nights=minimum_nights, plot_generated_neigh='NA',plot_generated_room='NA', price='')
 
 
+# this is to perform price prediction functionality on click of predict price button
 @app.route('/PredictPrice', methods=['GET', 'POST'])
 def predict_price():
     # to refill the dropdown values
@@ -246,18 +271,21 @@ def predict_price():
     roomtypes = n_df.room_type.unique()
     sel_roomtype = selected_roomtype
 
+    # label encode provinces
     label_encoder = preprocessing.LabelEncoder()
     label_encoder.fit(ndf['province'])
     le_province_mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
     selected_province = le_province_mapping[str(selected_province)]
     ndf['province'] = label_encoder.transform(ndf['province'])
 
+    # label encode room types
     label_encoder = preprocessing.LabelEncoder()
     label_encoder.fit(ndf['room_type'])
     le_room_type_mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
     selected_roomtype = le_room_type_mapping[selected_roomtype]
     ndf['room_type'] = label_encoder.transform(ndf['room_type'])
 
+    # label encode neighbourhoods
     label_encoder = preprocessing.LabelEncoder()
     label_encoder.fit(ndf['neighbourhood'])
     le_neighbourhood_mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
@@ -267,18 +295,22 @@ def predict_price():
     data = {}
     tdf = pd.DataFrame(data)
 
+    # filter dataframe using selected user parameters
     neighbourhood_list = (ndf[ndf.province == selected_province].neighbourhood.unique())
+    # for all other neighborhoods along with selected neighborhood
     for val in neighbourhood_list:
         data = {'neighbourhood': [val], 'province': [selected_province], 'room_type': [selected_roomtype],
                 'accommodates': [int(selected_accommodates)]}
         tdf = tdf.append(pd.DataFrame(data), ignore_index=True)
 
+    # for all other roomtypes along with selected roomtype
     for val in roomtypes:
         if val != sel_roomtype:
             data = {'neighbourhood': [selected_neighbourhood], 'province': [selected_province],
                     'room_type': [le_room_type_mapping[val]], 'accommodates': [int(selected_accommodates)]}
             tdf = tdf.append(pd.DataFrame(data), ignore_index=True)
 
+    # handling optional parameters - minimum_nights, bedrooms
     sel_min = ''
     sel_bedrooms = ''
     if selected_bedrooms != 'b0' and selected_min_nights != 'm0':
@@ -295,20 +327,8 @@ def predict_price():
         tdf['minimum_nights'] = int(selected_min_nights)
         sel_min = int(selected_min_nights)
 
+    # performing XgBoost
     xgb_reg = xgb.XGBRegressor(max_depth=5, min_child_weight=24)
-
-    # to generate residual plot
-    img = io.BytesIO()
-    X_train, X_test, y_train, y_test = train_test_split(ndf[tdf.columns], ndf.price, test_size=0.3, random_state=10)
-    xgb_reg.fit(X_train, y_train)
-    visualizer = ResidualsPlot(xgb_reg)
-    visualizer.fit(X_train, y_train)  # Fit the training data to the visualizer
-    visualizer.score(X_test, y_test)
-    residuals_plot = visualizer.show()
-    fig = residuals_plot.get_figure()
-    fig.savefig(img, format='png')
-    img.seek(0)
-    residual_plot = 'data:image/png;base64,{}'.format(base64.b64encode(img.getvalue()).decode())
 
     # to plot regression line
     img2 = io.BytesIO()
@@ -328,6 +348,7 @@ def predict_price():
     img2.seek(0)
     regression_line = 'data:image/png;base64,{}'.format(base64.b64encode(img2.getvalue()).decode())
 
+    # Xgboost on only selected parameters to predict price
     xgb_reg = xgb.XGBRegressor(max_depth=5, min_child_weight=24)
     xgb_reg.fit(ndf[tdf.columns], ndf.price)
     tdf['price'] = xgb_reg.predict(tdf)
@@ -373,10 +394,11 @@ def predict_price():
                            plot_generated_room=plot_generated_room, sel_province=request.form.get('provinceID'),
                            sel_neighbourhood=sel_neighbourhood, sel_roomtype=sel_roomtype,sel_min=sel_min,
                            sel_bedrooms=sel_bedrooms, sel_accommodates=int(request.form.get('accommodateID')),
-                           residual_plot=residual_plot,feature_importance_plot=feature_importance_plot,
+                           feature_importance_plot=feature_importance_plot,
                            regression_line=regression_line)
 
 
+# this is to generate plots for other neighborhoods and other roomtypes
 def get_price_plots(recommended, plot_type):
     x = list(recommended.keys())
     y = list(recommended.values())
@@ -403,10 +425,10 @@ def get_price_plots(recommended, plot_type):
     graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graph_json
 
-
 # *********************************************** Clustering page ***************************************************
 
 
+# route to return clustering page
 @app.route('/Clustering', methods=['GET'])
 def render_clustering():
     return render_template('clustering.html')
@@ -443,6 +465,7 @@ def constructDistanceMatrixUrl(dataFrame, latitude, longitude):
     return url
 
 
+# function that clusters the nearest aibnbs.
 def clusterData(latitude, longitude, pointOfInterest):
     df_entire = main_class_obj.cleaned_df
     timeList = []
@@ -479,6 +502,7 @@ def clusterData(latitude, longitude, pointOfInterest):
     return jsonData
 
 
+# this endpoint will be called when a user selects the point of interest
 @app.route('/getData')
 def get_data():
     latitude = request.args.get('lat')
@@ -487,10 +511,6 @@ def get_data():
     longitude = float(longitude)
     pointOfInterest = (latitude, longitude)
     outputData = clusterData(latitude, longitude, pointOfInterest)
-    with open('json.json', 'w') as f:
-        f.write(outputData)
-    # with open('json.json', 'r') as content_file:
-    #     outputData = content_file.read()
+    # with open('json.json', 'w') as f:
+    #     f.write(outputData)
     return outputData
-
-
