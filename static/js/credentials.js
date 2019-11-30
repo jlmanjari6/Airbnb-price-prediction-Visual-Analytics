0b1f6984d747459513318506d480e88c5a247665
Manjari <lk659879@dal.ca>
2019-11-30 14:45:28 -0400

    api keys updated

--- static/js/credentials.js
+++ static/js/credentials.js
@@ -1,2 +1,2 @@
-const googleMapAPIKey = '';
-const distanceMatrixAPI = '';
+const googleMapAPIKey = 'AIzaSyDQQe7eT78OgKffL6l6CMl3mPrD1O_f8SA';
+const distanceMatrixAPI = 'AIzaSyDPfqXnMS3gq-kGceSKgBs1rcoaqYYVJag';
