d1baccbd5bbbe9ab4de02733007a4ee9e91d59b3
Manjari <40268795+jlmanjari6@users.noreply.github.com>
2019-12-17 14:24:24 -0400

    Update credentials.js

--- static/js/credentials.js
+++ static/js/credentials.js
@@ -1,2 +1,2 @@
-const googleMapAPIKey = 'AIzaSyDQQe7eT78OgKffL6l6CMl3mPrD1O_f8SA';
-const distanceMatrixAPI = 'AIzaSyDPfqXnMS3gq-kGceSKgBs1rcoaqYYVJag';
+const googleMapAPIKey = '---add your API key here---';
+const distanceMatrixAPI = '---add your API key here---';
